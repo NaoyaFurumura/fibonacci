e5dcd4b6939782f6e50470ce0d51f0fb77fb7340
naoyafurumura <naoyafurumura@naoyafuuranoMBP.AirPort>
2021-05-30 17:46:28 +0900

    アルゴリズム改善

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+function fib(n) {
+  if (memo.has(n)) {
+    return memo.get(n);
+  }
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value);
+  return value;
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
